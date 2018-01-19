49bd277427c3f3ae2118c63bf07da334e8d2b307
gocar <962722173@qq.com>
2018-01-19 17:55:56 +0800

    未知错误只在debug模式下弹出

--- technet/src/main/java/com/ecar/ecarnetwork/http/api/ApiBox.java
+++ technet/src/main/java/com/ecar/ecarnetwork/http/api/ApiBox.java
@@ -324,7 +324,7 @@ public class ApiBox {
     private Gson getReponseGson() {
         Gson gson = new GsonBuilder()
                 .serializeNulls()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)//会把字段首字母大写
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)//会把字段首字母大写
                 .create();
         return gson;
     }
